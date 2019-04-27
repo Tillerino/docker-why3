5c40cc400b5b2fa49ce96730eced73b664ea3abe
Tillmann Gaida <tillmann.gaida@gmail.com>
2019-04-27 15:40:18 +0200

    Add a pseudo binary to make why3 feel more like it's running on the host machine

--- startup.sh
+++ startup.sh
@@ -1,4 +1,14 @@
 #!/bin/bash
 
 ARGS="$@"
-eval $(opam env) && why3 $ARGS
+
+CONFIG=
+if [ -d /conf ]; then
+  echo Using config from host system
+  if [ ! -f /conf/.why3.conf ]; then
+    cp $HOME/.why3.conf /conf/
+  fi
+  CONFIG="-C /conf/.why3.conf"
+fi
+
+eval $(opam env) && why3 $CONFIG $ARGS
